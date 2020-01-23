366525ec173c798bf868799d09b6ec1718faa246
Nick Buzzy <nick.buzzy@chmuraecon.com>
2020-01-23 13:25:38 -0500

    Integrate forecast and geocode functions into app; wire up weather route with these functions.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 const app = express();
 
@@ -46,23 +48,24 @@ app.get('/weather', (req, res) => {
         });
     }
 
-    res.send({
-        address: 'Philadelphia',
-        forecast: 'It is 70 degrees outside'
-    });
+    geocode(req.query.address, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
 });
-
-// app.get('/products', (req, res) => {
-//     if (!req.query.search) {
-//         return res.send({
-//             error: 'You must provide a search term'
-//         });
-//     }
-
-//     res.send({
-//         products: []
-//     })
-// })
 
 app.get('/help/*', (req, res) => {
     res.render('404', {
